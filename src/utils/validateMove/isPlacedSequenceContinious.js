49c8b388ec9d2fb48f087a2b165b568a00b757da
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2019-04-07 17:54:30 +0200

    added isPlacedSequenceContinious tests

--- src/utils/validateMove/isPlacedSequenceContinious.js
+++ src/utils/validateMove/isPlacedSequenceContinious.js
@@ -1,5 +1,25 @@
-const isPlacedSequenceCotinious = (placedIndices, squares) => {
+const isPlacedSequenceContinious = (placedIndices, step, squares) => {
+  if (placedIndices.length === 0) {
+    return true;
+  }
 
+  let index = placedIndices[0];
+  const indices = new Set(placedIndices);
+
+  while (index < squares.length && indices.size > 0) {
+    if (squares[index].letter === null || squares[index].letter === undefined) {
+      break;
+    }
+
+    indices.delete(index);
+    index = index + step;
+  }
+
+  if (indices.size === 0) {
+    return true;
+  }
+
+  return false;
 }
 
-export default isPlacedSequenceCotinious;
+export default isPlacedSequenceContinious;
