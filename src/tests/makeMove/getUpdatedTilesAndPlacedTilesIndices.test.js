e204cb5b10dca33ca572a9463b038767b6f42e85
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2019-04-14 19:43:42 +0200

    added tests

---
+++ src/tests/makeMove/getUpdatedTilesAndPlacedTilesIndices.test.js
@@ -0,0 +1,167 @@
+import getUpdatedTilesAndPlacedTilesIndices from '../../utils/makeMove/getUpdatedTilesAndPlacedTilesIndices';
+import getAnchorIndices from '../../utils/makeMove/getAnchorIndices';
+import getWordsAtAnchor from '../../utils/makeMove/getWordsAtAnchor';
+import getWordBuildStepFromAnchor from '../../utils/makeMove/getWordBuildStepFromAnchor';
+import isValidWordPlacement from '../../utils/validateMove/isValidWordPlacement';
+
+jest.mock('../../utils/makeMove/getAnchorIndices');
+jest.mock('../../utils/makeMove/getWordsAtAnchor');
+jest.mock('../../utils/makeMove/getWordBuildStepFromAnchor');
+jest.mock('../../utils/validateMove/isValidWordPlacement');
+
+// https://stackoverflow.com/questions/41570273/how-to-test-a-function-that-output-is-random-using-jest
+const mockMath = Object.create(global.Math);
+mockMath.random = () => 0;
+global.Math = mockMath;
+
+describe('getUpdatedTilesAndPlacedTilesIndices', () => {
+  afterEach(() => {
+    getAnchorIndices.mockClear();
+    getWordsAtAnchor.mockClear();
+    getWordBuildStepFromAnchor.mockClear();
+    isValidWordPlacement.mockClear();
+  });
+
+  describe('when no valid word placement', () => {
+    it('returns null', () => {
+      const tiles = [
+        {}, {}, {}, {}, {},
+        {}, {}, {}, {}, {},
+        {}, {}, {}, {}, {},
+        {}, {}, {}, {}, {},
+        {}, {}, {}, {}, {},
+      ];
+      const boardSize = 5;
+      const dictionary = {};
+      const rack = [];
+
+      getAnchorIndices.mockImplementation(() => [0]);
+
+      getWordBuildStepFromAnchor.mockImplementation(() => 0);
+
+      const wordsAtAnchor = [
+        [
+          {index: 0, letter: 'b'},
+          {index: 5, letter: 'b'},
+          {index: 10, letter: 'a'},
+          {index: 15, letter: 'a'},
+        ],        
+        [
+          {index: 7, letter: 'b'},
+          {index: 12, letter: 'a'},
+          {index: 17, letter: 'b'},
+          {index: 22, letter: 'b'},
+        ],
+      ];
+      getWordsAtAnchor.mockImplementation(() => wordsAtAnchor);
+
+      isValidWordPlacement.mockImplementation(() => {
+        return {isValid: false};
+      });
+
+      const {updatedTiles, placedTilesIndices} = getUpdatedTilesAndPlacedTilesIndices(tiles, boardSize, rack, dictionary);   
+
+      expect(placedTilesIndices).toBeNull();
+      expect(updatedTiles).toBeNull();
+    });
+  });
+
+  describe('when finds valid word placement', () => {
+    it('returns updated tiles and placed tiles indices', () => {
+      const tiles = [
+        {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+      ];
+      const boardSize = 5;
+      const dictionary = {'a': ['a']};
+      const rack = [{letter: 'a'}, {letter: 'b'}, {letter: 'c'}, {letter: 'd'}];
+
+      const anchorIndices = [5, 6, 7, 8, 9];
+      getAnchorIndices.mockImplementation(() => anchorIndices);
+
+      const step = 5;
+      getWordBuildStepFromAnchor.mockImplementation(() => step);
+
+      const wordsAtAnchor = [
+        [
+          {index: 0, letter: 'b'},
+          {index: 5, letter: 'b'},
+          {index: 10, letter: 'a'},
+          {index: 15, letter: 'a'},
+        ],        
+        [
+          {index: 7, letter: 'b'},
+          {index: 12, letter: 'a'},
+          {index: 17, letter: 'b'},
+          {index: 22, letter: 'b'},
+        ],
+      ];
+      getWordsAtAnchor.mockImplementation(() => wordsAtAnchor);
+
+      isValidWordPlacement.mockImplementation((tiles, boardSize, placedTilesIndices, dictionary) => {
+        if (placedTilesIndices.length === 2) {
+          if (placedTilesIndices[0] === 0 && placedTilesIndices[1] === 5) {
+            return {isValid: false};
+          }
+        }
+
+        return {isValid: true};
+      });
+
+      const expectedUpdatedTiles = [
+        {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: null}, {letter: null}, {letter: {letter: 'b'}}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: null}, {letter: {letter: 'b'}}, {letter: null}, {letter: null}, 
+        {letter: null}, {letter: null}, {letter: {letter: 'b'}}, {letter: null}, {letter: null}, 
+      ];
+
+      const {updatedTiles, placedTilesIndices} = getUpdatedTilesAndPlacedTilesIndices(tiles, boardSize, rack, dictionary);   
+      
+      expect(getAnchorIndices.mock.calls.length).toBe(1);
+      expect(getAnchorIndices.mock.calls[0][0]).toEqual(tiles);
+      expect(getAnchorIndices.mock.calls[0][1]).toEqual(boardSize);
+
+      expect(getWordBuildStepFromAnchor.mock.calls.length).toBe(1);
+      expect(getWordBuildStepFromAnchor.mock.calls[0][0]).toEqual(tiles);
+      expect(getWordBuildStepFromAnchor.mock.calls[0][1]).toEqual(boardSize);
+      expect(getWordBuildStepFromAnchor.mock.calls[0][2]).toEqual(5);
+
+      expect(getWordsAtAnchor.mock.calls.length).toBe(1);
+      expect(getWordsAtAnchor.mock.calls[0][0]).toEqual(tiles);
+      expect(getWordsAtAnchor.mock.calls[0][1]).toEqual(rack);
+      expect(getWordsAtAnchor.mock.calls[0][2]).toEqual(dictionary);
+      expect(getWordsAtAnchor.mock.calls[0][3]).toEqual(5);
+      expect(getWordsAtAnchor.mock.calls[0][4]).toEqual(boardSize);
+      expect(getWordsAtAnchor.mock.calls[0][5]).toEqual(step);
+
+      expect(isValidWordPlacement.mock.calls.length).toBe(2);
+      expect(isValidWordPlacement.mock.calls[0][0]).toEqual([
+        {letter: {letter: 'b'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'b'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+      ]);
+      expect(isValidWordPlacement.mock.calls[0][1]).toEqual(boardSize);
+      expect(isValidWordPlacement.mock.calls[0][2]).toEqual([0, 5]);
+      expect(isValidWordPlacement.mock.calls[0][3]).toEqual(dictionary);
+      expect(isValidWordPlacement.mock.calls[1][0]).toEqual([
+        {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, 
+        {letter: null}, {letter: null}, {letter: {letter: 'b'}}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: {letter: 'a'}}, {letter: null}, {letter: null}, 
+        {letter: {letter: 'a'}}, {letter: null}, {letter: {letter: 'b'}}, {letter: null}, {letter: null}, 
+        {letter: null}, {letter: null}, {letter: {letter: 'b'}}, {letter: null}, {letter: null}, 
+      ]);
+      expect(isValidWordPlacement.mock.calls[1][1]).toEqual(boardSize);
+      expect(isValidWordPlacement.mock.calls[1][2]).toEqual([7, 17, 22]);
+      expect(isValidWordPlacement.mock.calls[1][3]).toEqual(dictionary);
+
+      expect(placedTilesIndices).toEqual([7, 17, 22]);
+      expect(updatedTiles).toEqual(expectedUpdatedTiles);
+    });
+  });
+});
---
+++ src/utils/makeMove/getUpdatedTilesAndPlacedTilesIndices.js
@@ -0,0 +1,44 @@
+import getAnchorIndices from './getAnchorIndices';
+import getWordsAtAnchor from './getWordsAtAnchor';
+import getWordBuildStepFromAnchor from './getWordBuildStepFromAnchor';
+import isValidWordPlacement from '../validateMove/isValidWordPlacement';
+
+const getUpdatedTilesAndPlacedTilesIndices = (tiles, boardSize, rack, dictionary) => {
+  const anchorIndices = getAnchorIndices(tiles, boardSize);
+
+  if (anchorIndices.length === 0) {
+    return [];
+  }
+
+  const anchorIndex = anchorIndices[Math.floor(Math.random() * anchorIndices.length)];
+
+  const step = getWordBuildStepFromAnchor(tiles, boardSize, anchorIndex);
+
+  const wordsAtAnchor = getWordsAtAnchor(tiles, rack, dictionary, anchorIndex, boardSize, step);
+  for (let word of wordsAtAnchor) {
+    const updatedTiles = JSON.parse(JSON.stringify(tiles));
+    const placedTilesIndices = [];
+    for (let letter of word) {
+      if (updatedTiles[letter.index].letter !== null && updatedTiles[letter.index] !== undefined) {
+        continue;
+      }
+
+      updatedTiles[letter.index].letter = {letter: letter.letter};
+      placedTilesIndices.push(letter.index);
+    }
+    const {isValid, _} = isValidWordPlacement(updatedTiles, boardSize, placedTilesIndices, dictionary);
+    if (isValid === true) {
+      return {
+        updatedTiles: updatedTiles,
+        placedTilesIndices: placedTilesIndices
+      }
+    }
+  }
+
+  return {
+    updatedTiles: null,
+    placedTilesIndices: null
+  }
+};
+
+export default getUpdatedTilesAndPlacedTilesIndices;
