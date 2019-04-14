66ab138292f79b239892f41d2576a956858d6edf
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2019-04-14 13:58:55 +0200

    added combinatorics package and first minimaly working version of make move

--- src/tests/makeMove/getWordAtAnchor.test.js
+++ src/tests/makeMove/getWordAtAnchor.test.js
@@ -1,40 +1,103 @@
 import getWordAtAnchor from '../../utils/makeMove/getWordAtAnchor';
 
 describe('getWordAtAnchor', () => {
-  describe('returns expected word', () => {
-    const squares = [
-      {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 't'}},
-      {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'm'}}, {letter: {letter: 'a'}}, {letter: {letter: 'n'}}, {letter: {letter: 'g'}}, {letter: {letter: 'o'}},
-      {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'o'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
-      {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'n'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
-      {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'e'}}, {letter: {letter: 'a'}}, {letter: {letter: 'r'}}, {letter: {letter: 's'}}, {letter: null},
-      {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'y'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
-      {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
-      {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}
-    ];
-    const stepVertical = 8;
-    const anchorIndex = 26;
-    const dictionary = {
-      'aden': ['dean']
-    };
-    const rack = [
-      {letter: 'd'},
-      {letter: 'e'},
-      {letter: 'a'},
-      {letter: 't'},
-      {letter: 'r'},
-      {letter: 'e'},
-      {letter: 'e'},
-    ]
+  const squares = [
+    {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 't'}},
+    {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'm'}}, {letter: {letter: 'a'}}, {letter: {letter: 'n'}}, {letter: {letter: 'g'}}, {letter: {letter: 'o'}},
+    {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'o'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
+    {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'n'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
+    {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'e'}}, {letter: {letter: 'a'}}, {letter: {letter: 'r'}}, {letter: {letter: 's'}}, {letter: null},
+    {letter: null}, {letter: null}, {letter: null}, {letter: {letter: 'y'}}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
+    {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null},
+    {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}, {letter: null}
+  ];
+  const stepVertical = 8;
+  const anchorIndex = 26;
 
-    const expected = [
-      {index: 24, letter: 'd'},
-      {index: 25, letter: 'e'},
-      {index: 26, letter: 'a'},
-    ];
+  const rack = [
+    {letter: 'd'},
+    {letter: 'e'},
+    {letter: 'a'},
+    {letter: 't'},
+    {letter: 'r'},
+    {letter: 'e'},
+    {letter: 'e'},
+  ];
 
-    const actual = getWordAtAnchor(squares, rack, dictionary, anchorIndex, stepVertical);
 
-    expect(actual).toEqual(expected);
+  describe('when step is horizontal', () => {
+    const step = 1;
+
+    describe('when there is a single match in the dictionary', () => {
+      it('returns expected word', () => {
+        const dictionary = {
+          'aden': ['dean'],
+          'zean': ['zean'],
+          'eert': ['tree', 'reet']
+        };
+    
+        const expected = [
+          [
+            {index: 24, letter: 'd'},
+            {index: 25, letter: 'e'},
+            {index: 26, letter: 'a'},
+            {index: 27, letter: 'n'},
+          ],
+        ];
+    
+        const actual = getWordAtAnchor(squares, rack, dictionary, anchorIndex, stepVertical, step);
+        expect(actual).toEqual(expected);
+      });
+  
+      describe('when the match does not fit in the placeholder', () => {
+        it('returns empty array', () => {
+          const dictionary = {
+            'aden': ['nade'],
+            'zean': ['zean'],
+            'eert': ['tree', 'reet']
+          };
+          const expected = [];
+      
+          const actual = getWordAtAnchor(squares, rack, dictionary, anchorIndex, stepVertical, step);
+          expect(actual).toEqual(expected);
+        });
+      });
+    });
+  
+    describe('when there are multiple matches in the dictionary', () => {
+      it('returns expected words', () => {
+        const dictionary = {
+          'aden': ['dean', 'aden'],
+          'zean': ['zean'],
+          'eert': ['tree', 'reet'],
+          'aednt': ['denta']
+        };
+  
+        const expected = [
+          [
+            {index: 25, letter: 'd'},
+            {index: 26, letter: 'e'},
+            {index: 27, letter: 'n'},
+            {index: 28, letter: 't'},
+            {index: 29, letter: 'a'},
+          ],
+          [
+            {index: 24, letter: 'd'},
+            {index: 25, letter: 'e'},
+            {index: 26, letter: 'a'},
+            {index: 27, letter: 'n'},
+          ],
+          [
+            {index: 24, letter: 'a'},
+            {index: 25, letter: 'd'},
+            {index: 26, letter: 'e'},
+            {index: 27, letter: 'n'},
+          ]
+        ];
+    
+        const actual = getWordAtAnchor(squares, rack, dictionary, anchorIndex, stepVertical, step);
+        expect(actual).toEqual(expected);
+      });
+    });
   });
 });
--- src/utils/makeMove/getWordAtAnchor.js
+++ src/utils/makeMove/getWordAtAnchor.js
@@ -1,16 +1,122 @@
 import indexToRowCol from '../board/indexToRowCol';
+import Combinatorics from 'js-combinatorics';
 
-const getWordAtAnchor = (squares, rack, dictionary, anchorIndex, boardSize) => {
+const getWordAtAnchor = (tiles, rack, dictionary, anchorIndex, boardSize, step) => {
   // if (anchors.length === 0) {
   //   return null;
   // }
 
   // const anchorIndex = anchors[Math.floor(Math.random() * anchors.length)];
 
-  const rackLetters =  rack.map(x => x.letter).sort();
+  const rackLetters =  rack.map(x => x.letter);
 
   const rowCol = indexToRowCol(anchorIndex, boardSize);
+  const anchorRow = rowCol.row;
+  const anchorCol = rowCol.col;
 
+  const placeholderBack = getPlaceholder(tiles, -step, anchorIndex, anchorRow, anchorCol, boardSize);
+  const placeholderForward = getPlaceholder(tiles, step, anchorIndex, anchorRow, anchorCol, boardSize);
+  placeholderForward.unshift(null);
+  const placeholderAnchorIndex = placeholderBack.length;
+  const placeHolder = placeholderBack.reverse().concat(placeholderForward);
+  const placeHolderLetters = placeHolder.filter(x => x !== null);
+
+  const letters = rackLetters.concat(placeHolderLetters);
+
+  const maxWordLength = Math.min(placeHolder.length, letters.length);
+
+  const candidates = [];
+  for (let length = maxWordLength; length > 1; length--) {
+    const combinations = Combinatorics.combination(letters, length);
+    let combination;
+    const seenCombinations = new Set();
+    while (combination = combinations.next()) {
+      combination = combination.sort().join('');
+      if (seenCombinations.has(combination) === true) {
+        continue;
+      }
+      seenCombinations.add(combination);
+
+      const words = dictionary[combination];
+      if (words === null || words === undefined) {
+        continue;
+      }
+      for (const word of words) {
+        const indicesInPlaceholder = getIndicesInPlaceholder(placeHolder, word);
+        if (indicesInPlaceholder === null) {
+          continue;
+        }
+
+        if (indicesInPlaceholder.length !== word.length) {
+          continue;
+        }
+
+        const indicesInBoard = indicesInPlaceholder.map(i => anchorIndex + (i - placeholderAnchorIndex) * step);
+
+        const candidateWord = [];
+        for (let i = 0; i < indicesInBoard.length; i++) {
+          candidateWord.push({
+            index: indicesInBoard[i],
+            letter: word[i]
+          })
+        }
+
+        candidates.push(candidateWord);
+      }
+    }
+  }
+
+  return candidates;  
+};
+
+const getIndicesInPlaceholder = (placeholder, word) => {
+  for (let i = 0; i < placeholder.length - word.length; i++) {
+    const placeholderSlice = placeholder.slice(i, i+word.length);
+    let canFit = true;
+    for (let j = 0; j < word.length; j++) {
+      if (placeholderSlice[j] === null) {
+        continue;
+      }
+
+      if (word[j] !== placeholderSlice[j]) {
+        canFit = false;
+        break;
+      }
+    }
+
+    if (canFit === true) {
+      const indices = [];
+      for (let l = i; l < word.length; l++) {
+        indices.push(l);
+      }
+      return indices;
+    }
+  }
+
+  return null;
+}
+
+const getPlaceholder = (squares, step, anchorIndex, anchorRow, anchorCol, boardSize) => {
+  let index = anchorIndex + step;
+  const rowCol = indexToRowCol(index, boardSize);
+  let row = rowCol.row;
+  let col = rowCol.col;
+
+  const placeholder = [];
+  while (row === anchorRow || col === anchorCol) {
+    if (squares[index].letter === null || squares[index].letter === undefined) {
+      placeholder.push(null);
+    } else {
+      placeholder.push(squares[index].letter.letter);
+    }
+
+    index = index + step;
+    const rowCol = indexToRowCol(index, boardSize);
+    row = rowCol.row;
+    col = rowCol.col;
+  }
+
+  return placeholder;
 };
 
 export default getWordAtAnchor;
