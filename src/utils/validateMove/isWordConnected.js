80ecb65e026b3f7d31cf9103e9d9cad428d24371
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2019-04-25 22:25:09 +0200

    optimized isWordConnected

--- src/utils/validateMove/isWordConnected.js
+++ src/utils/validateMove/isWordConnected.js
@@ -1,23 +1,31 @@
-const isWordConnected = (wordIndices, step_horizontal, step_vertical, squares) => {
+const isWordConnected = (wordIndices, step_horizontal, step_vertical, tiles) => {
+  const tilesLength = tiles.length;
   let alreadyPlayedPresent = false;
-  for (let letter of squares.map(sq => sq.letter).filter(l => l !== null && l !== undefined)) {
+
+  for (let i = 0; i < tilesLength; i++) {
+    const letter = tiles[i].letter;
+
+    if (letter === null || letter === undefined) {
+      continue;
+    }
+
     if (letter.alreadyPlayed === true) {
       alreadyPlayedPresent = true;
       break;
-    }    
+    }
   }
 
   if (alreadyPlayedPresent === false) {
     return true;
   }
-  const squaresLength = squares.length;
+
   const modifiers = [step_horizontal, -step_horizontal, step_vertical, -step_vertical];
   for (let index of wordIndices) {
     for (let modifier of modifiers) {
       const index_modified = index + modifier;
-      if (index_modified >= 0 && index_modified < squaresLength) {
-        if (squares[index_modified].letter !== null && squares[index_modified].letter !== undefined) {
-          if (squares[index_modified].letter.alreadyPlayed === true) {
+      if (index_modified >= 0 && index_modified < tilesLength) {
+        if (tiles[index_modified].letter !== null && tiles[index_modified].letter !== undefined) {
+          if (tiles[index_modified].letter.alreadyPlayed === true) {
             return true;
           }
         }
