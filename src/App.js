66f117399743926f379b34619d3bfcf209d1d8fa
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2019-05-30 21:09:08 +0200

    added language switcher

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Game from './containers/Game/Game';
 import { IntlProvider } from 'react-intl';
 import messages from './messages';
+import Button from './components/UI/Button/Button';
 
 class App extends Component {
   constructor(props) {
@@ -12,11 +13,32 @@ class App extends Component {
     }
   }
 
+  setEnglishLangHandler = () => {
+    this.setState({
+      'lang': 'en'
+    });
+  }
+
+
+  setUkrainianLangHandler = () => {
+    this.setState({
+      'lang': 'uk'
+    });
+  }
+
   render() {
     const lang = this.state.lang;
     return (
       <IntlProvider locale={lang} messages={messages[lang]}>
         <div className="App">
+          <Button 
+            caption='English'
+            clickHandler={this.setEnglishLangHandler}
+          />
+          <Button 
+            caption='Українською'
+            clickHandler={this.setUkrainianLangHandler}
+          />
           <Game />        
         </div>
       </IntlProvider>
