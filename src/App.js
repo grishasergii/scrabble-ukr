a8a7bfe7a2991043594ec992ca332f1335516f06
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2019-05-30 23:29:56 +0200

    styled error message

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ class App extends Component {
         <div className="App">
           
           <Game />     
-        
+
+
+
           <ToggleButton 
             handler={this.toggleLangHandler}
             isToggleOn={true}
