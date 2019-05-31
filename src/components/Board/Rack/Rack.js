224dd96e5c499cc3ae7db69309be025e4cd6c29e
Sergii Gryshkevych <s.gryshkevych@gmail.com>
2019-05-31 22:54:07 +0200

    deconstructed props in the rack component

--- src/components/Board/Rack/Rack.js
+++ src/components/Board/Rack/Rack.js
@@ -4,17 +4,17 @@ import Square from '../Square/Square';
 import Letter from '../Letter/Letter';
 
 
-const rack = (props) => {
-  const squares = props.letters.map((l, index) => {
+const rack = ({letters, rackSelectable, letterClick, selectedFrom}) => {
+  const squares = letters.map((l, index) => {
     let letter = null;
     if (l !== null && l !== undefined) {
       letter = (
         <Letter 
-          selectable={props.rackSelectable}
+          selectable={rackSelectable}
           letter={l.letter} 
           value={l.value}
           selected={l.selected}
-          clicked={() => props.letterClick(l, index, props.selectedFrom)}
+          clicked={() => letterClick(l, index, selectedFrom)}
           color={l.color} />
       );
     }
